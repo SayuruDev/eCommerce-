5d839d38b4b897e5912741dc523a401b2e9a4ae2
Sayuru Dev <mailtosayuru@gmail.com>
2025-07-31 03:29:35 -0400

    first commit

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
+
+dotenv.config({ path: "backend/config/config.env" });
+
+connectDatabase();
+app.use(express.json());
+
+
+import productRoutes from "./routes/products.js";
+
+app.use("/api/v1", productRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
