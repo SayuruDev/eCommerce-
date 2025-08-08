f6f49934ecdec2b7138f56f2cb6864a3f2597aec
Sayuru Dev <mailtosayuru@gmail.com>
2025-08-08 02:29:21 -0400

    products added backend

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,66 +1,92 @@
-import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
-import Product from "../models/product.js";
-import ErrorHandler from "../utils/errorHandler.js";
+import mongoose from "mongoose";
 
-// Create new Product   =>  /api/v1/products
-export const getProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+      maxLength: [200, "Product name cannot exceed 200 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter product price"],
+      maxLength: [5, "Product price cannot exceed 5 digits"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter product description"],
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please enter product category"],
+      enum: {
+        values: [
+          "Electronics",
+          "Cameras",
+          "Laptops",
+          "Accessories",
+          "Headphones",
+          "Food",
+          "Books",
+          "Sports",
+          "Outdoor",
+          "Home",
+        ],
+        message: "Please select correct category",
+      },
+    },
+    seller: {
+      type: String,
+      required: [true, "Please enter product seller"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter product stock"],
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
-  res.status(200).json({
-    products,
-  });
-});
-
-// Create new Product   =>  /api/v1/admin/products
-export const newProduct = catchAsyncErrors(async (req, res) => {
-  const product = await Product.create(req.body);
-
-  res.status(200).json({
-    product,
-  });
-});
-
-// Get single product details   =>  /api/v1/products/:id
-export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
-  const product = await Product.findById(req?.params?.id);
-
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
-
-  res.status(200).json({
-    product,
-  });
-});
-
-// Update product details   =>  /api/v1/products/:id
-export const updateProduct = catchAsyncErrors(async (req, res) => {
-  let product = await Product.findById(req?.params?.id);
-
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
-
-  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
-    new: true,
-  });
-
-  res.status(200).json({
-    product,
-  });
-});
-
-// Delete product   =>  /api/v1/products/:id
-export const deleteProduct = catchAsyncErrors(async (req, res) => {
-  const product = await Product.findById(req?.params?.id);
-
-  if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
-  }
-
-  await product.deleteOne();
-
-  res.status(200).json({
-    message: "Product Deleted",
-  });
-});
+export default mongoose.model("Product", productSchema);
